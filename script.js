42922934a14f41ed975b44a9ccc7d78582913fc0
cyberck <dushaev2017@gmail.com>
2021-09-12 11:44:27 -0700

    poi

---
+++ script.js
@@ -0,0 +1,10 @@
+
+let asd = {
+
+};
+
+for (let i = 1; i < 11; i++) {
+    let imya = prompt('Vvedite imya'),
+        vozrost = +prompt('Vvedite vozrost')
+        console.log(`imya ${imya} vozrost ${vozrost}`); 
+}
